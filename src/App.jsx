092700cec15b6a024ba735eaa244af0199a23d11
muslimsislomjonova-cmd <muslimsislomjonova@gmail.com>
2026-05-02 20:06:01 +0500

    appjsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import UserList from './components/UserList';
+ 
+function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>RTK User Explorer</h1>
+        <UserList />
+      </div>
+    </Provider>
+  );
+}
+ 
+export default App;
