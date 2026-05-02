5e039931d3c6c3b528616beebfad71e554b8b724
muslimsislomjonova-cmd <muslimsislomjonova@gmail.com>
2026-05-02 20:58:45 +0500

    feature/appcss_etc

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import UserList from './components/UserList';
+import './App.css';
  
+
 function App() {
   return (
     <Provider store={store}>
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,35 +1,56 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../users/usersSlice';
- 
+
 function UserList() {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users.data);
   const loading = useSelector((state) => state.users.loading);
   const error = useSelector((state) => state.users.error);
- 
+
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
- 
+
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="status">
+        <div className="spinner"></div>
+        <p>Yuklanmoqda...</p>
+      </div>
+    );
   }
- 
+
   if (error) {
-    return <p style={{ color: 'red' }}>{error}</p>;
+    return (
+      <div className="status">
+        <div className="error-box">
+          <p>{error}</p>
+        </div>
+      </div>
+    );
   }
- 
+
   return (
-    <div>
-      <h2>Foydalanuvchilar royxati</h2>
-      <ul>
+    <div className="user-list-wrapper">
+      <h2>Foydalanuvchilar <span className="count">{users.length}</span></h2>
+      <ul className="user-list">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className="user-card">
+            <div className="avatar">{user.name.charAt(0)}</div>
+            <div className="user-info">
+              <span className="name">{user.name}</span>
+              <span className="email">{user.email}</span>
+              <span className="extra"> {user.phone}</span>
+              <span className="extra"> {user.username}</span>
+              <span className="extra"> {user.website}</span>
+              <span className="extra">{user.address.city}</span>
+            </div>
+          </li>
         ))}
       </ul>
     </div>
   );
 }
- 
+
 export default UserList;
