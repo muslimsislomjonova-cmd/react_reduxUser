31be2140eb73d02ca4156fc1e5c086c8a8c7ee13
muslimsislomjonova-cmd <muslimsislomjonova@gmail.com>
2026-05-02 20:24:38 +0500

    feature/storejs

--- src/app/store.js
+++ src/app/store.js
@@ -1,37 +1,10 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-
-export const fetchUsers = createAsyncThunk(
-  'users/fetchUsers',
-  async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await response.json();
-    return data;
-  }
-);
-
-const usersSlice = createSlice({
-  name: 'users',
-  initialState: {
-    data: [],
-    loading: false,
-    error: null,
-  },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchUsers.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(fetchUsers.fulfilled, (state, action) => {
-        state.loading = false;
-        state.data = action.payload;
-      })
-      .addCase(fetchUsers.rejected, (state) => {
-        state.loading = false;
-        state.error = 'Xatolik yuz berdi';
-      });
+import { configureStore } from '@reduxjs/toolkit';
+import usersReducer from '../users/usersSlice';
+ 
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
   },
 });
-
-export default usersSlice.reducer;
+ 
+export default store;
